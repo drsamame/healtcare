3e0069920fafef6ea39408e6f382e406be117bc1
Diego Rodrigo Samame Salazar <tsm_dsamame@niubiz.com.pe>
2024-07-19 15:31:00 -0500

    advance register form with back and validations

---
+++ .history/components/FileUploader_20240717173643.tsx
@@ -0,0 +1,46 @@
+'use client';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
+import { convertFileToUrl } from "@/lib/utils";
+
+interface Props {
+	files: File[] | undefined;
+	onChange: (files: File[]) => void;
+}
+
+export const FileUploader = ({ files, onChange }: Props) => {
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		onChange(acceptedFiles);
+	}, []);
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+	return (
+		<div {...getRootProps()} className="file-upload">
+			<input {...getInputProps()} />
+			{files && files?.length > 0 ? (
+				<Image
+					src={convertFileToUrl(files[0])}
+					width={1000}
+					height={1000}
+					alt="upload image"
+					className="max-h-[400px] overflow-hidden object-cover"
+				/>
+			) : (
+				<>
+					<Image
+						src="/assets/icons/upload.svg"
+						alt="upload"
+						width={40}
+						height={40}
+					/>
+				</>
+			)}
+			{isDragActive ? (
+				<p>Drop the files here ...</p>
+			) : (
+				<p>Drag drop some files here, or click to select files</p>
+			)}
+		</div>
+	);
+};
